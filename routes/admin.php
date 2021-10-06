aed19843bd6f3cc28ff98be6a98d7a2b18612894
rk-2000 <ritikshri19@gmail.com>
2021-10-06 12:29:44 +0530

    Middleware added to routes

--- routes/admin.php
+++ routes/admin.php
@@ -24,41 +24,38 @@ Route::prefix('admin')->group(function () {
         Route::get('/dashboard', [App\Http\Controllers\Admin\HomeController::class, 'index'])->name('admin.home');
 
         //User
-        Route::get('/add-user',[App\Http\Controllers\Admin\AdminUserController::class,'addUser'])->name('add_user');
-        Route::get('/edit-user',[App\Http\Controllers\Admin\AdminUserController::class,'EditUser'])->name('edit_user');
-        Route::get('/user-list',[App\Http\Controllers\Admin\AdminUserController::class,'UserData'])->name('users_list');
-        Route::post('/edit-data',[App\Http\Controllers\Admin\AdminUserController::class,'EditData'])->name('admin.edit_data');
-        Route::post('/add-user-data',[App\Http\Controllers\Admin\AdminUserController::class,'addUserData'])->name('addUserData');
+        Route::middleware(['check.permissions:2'])->group(function () {
+            Route::get('/add-user', [App\Http\Controllers\Admin\AdminUserController::class, 'addUser'])->name('add_user');
+            Route::get('/edit-user', [App\Http\Controllers\Admin\AdminUserController::class, 'EditUser'])->name('edit_user');
+            Route::get('/user-list', [App\Http\Controllers\Admin\AdminUserController::class, 'UserData'])->name('users_list');
+            Route::post('/edit-data', [App\Http\Controllers\Admin\AdminUserController::class, 'EditData'])->name('admin.edit_data');
+            Route::post('/add-user-data', [App\Http\Controllers\Admin\AdminUserController::class, 'addUserData'])->name('addUserData');
+        });
 
         //Event
-        Route::get('/event-type', [App\Http\Controllers\Admin\EventController::class, 'eventType'])->name('event_type');
-        Route::get('/manage-event', [App\Http\Controllers\Admin\EventController::class, 'event'])->name('event');
-        Route::post('/add-event-type', [App\Http\Controllers\Admin\EventController::class, 'addEventType'])->name('addEventType');
-        Route::post('/manage-event', [App\Http\Controllers\Admin\EventController::class, 'manageEvent'])->name('manage_event');
+        Route::middleware(['check.permissions:4'])->group(function () {
+            Route::get('/event-type', [App\Http\Controllers\Admin\EventController::class, 'eventType'])->name('event_type');
+            Route::get('/manage-event', [App\Http\Controllers\Admin\EventController::class, 'event'])->name('event');
+            Route::post('/add-event-type', [App\Http\Controllers\Admin\EventController::class, 'addEventType'])->name('addEventType');
+            Route::post('/manage-event', [App\Http\Controllers\Admin\EventController::class, 'manageEvent'])->name('manage_event');
+        });
 
         //Roles
         Route::middleware(['check.permissions:11'])->group(function () {
             Route::get('/roles', [App\Http\Controllers\Admin\RoleController::class, 'manageRole'])->name('manage_role');
             Route::get('/add-role', [App\Http\Controllers\Admin\RoleController::class, 'addRole'])->name('add_role');
             Route::post('/add-role', [App\Http\Controllers\Admin\RoleController::class, 'addRoleData'])->name('admin.addRole.post');
-
-            // Update Role
             Route::get('/update-role', [App\Http\Controllers\Admin\RoleController::class, 'UpdateRole'])->name('update_role');
             Route::post('/update-role', [App\Http\Controllers\Admin\RoleController::class, 'UpdateRoleData'])->name('admin.UpdateRole.post');
-            // Delete Role
             Route::get('/delete-role', [App\Http\Controllers\Admin\RoleController::class, 'DeleteRoleData'])->name('admin.deleteRole');
         });
 
 
         //Prasad
         Route::middleware(['check.permissions:11'])->group(function () {
-            //Prasad
             Route::get('/manage-prasad', [App\Http\Controllers\Admin\PrasadController::class, 'index'])->name('prshad_type');
             Route::post('/manage-prasad', [App\Http\Controllers\Admin\PrasadController::class, 'addPrasadType'])->name('prshad_type_post');
-            //Delete Prasad
             Route::get('/delete-prasad', [App\Http\Controllers\Admin\PrasadController::class, 'DeletePrasadData'])->name('admin.deletePrasad');
-            //Update Prasad
-
             Route::post('/update-prasad', [App\Http\Controllers\Admin\PrasadController::class, 'UpdatePrasadData'])->name('update_prashad_type');
         });
 
