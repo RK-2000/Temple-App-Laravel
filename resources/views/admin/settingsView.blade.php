00046ec788830a37ac924a0e5dba3c9c43329130
SomanshuMishra <mishrasomanshu@gmail.com>
2021-10-07 13:23:49 +0530

    Settings Completed

--- resources/views/admin/settingsView.blade.php
+++ resources/views/admin/settingsView.blade.php
@@ -1,5 +1,14 @@
 @extends('admin.layout')
 
+@section('custom_scripts')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"
+    integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/1.11.2/js/jquery.dataTables.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css">
+@endsection
+
 @section('content')
 <style>
                       body {
@@ -22,63 +31,86 @@
                       </div>
                     
                     
-                    {{-- FORM START --}}
                     
-                    <form class="row  needs-validation" method="POST" action="{{route('settingsUpdate')}}" >
-                      @csrf
-                      <div class="col-md-6">
-                        <label for="name" class="form-label">Temple Name</label>
-                        <input type="text" class="form-control" id="name" name="temple_name" required>
-                        <div class="valid-feedback">
+
+                                {{-- Form Template Start--}}
+
+                                  <div class="row">
+
+                                    <div class="col-md-12 grid-margin stretch-card">
+                                      <div class="card">
+                                        <div class="card-body">
+
+                                          <form class="row needs-validation" method="POST" action="{{route('settingsUpdate')}}" enctype="multipart/form-data">
+                                            @csrf
+                                            <div class="form-group col-md-6">
+                                              <label for="temple_name">Temple Name</label>
+                                              <input type="text" class="form-control" name="temple_name" value="<?php echo $data['temple_name'] ?>" id="temple_name" placeholder="Temple Name">
+                                            </div>
+                                            <div class="form-group col-md-6">
+                                              <label for="address ">Address</label>
+                                              <input type="text" class="form-control" id="address" name="address" value="<?php echo $data['address'] ?>" placeholder="Address">
+                                            </div>
+                                            <div class="form-group col-md-6">
+                                              <label for="facebook_link">Facebook link</label>
+                                              <input type="text" class="form-control" id="facebook_link" name="facebook_link" value="<?php echo $data['facebook_link'] ?>"placeholder="Facebook link">
+                                            </div>
+                                            <div class="form-group col-md-6">
+                                              <label for="youtube_link">Youtube link</label>
+                                              <input type="text" class="form-control" id="youtube_link" name="youtube_link" value="<?php echo $data['youtube_link'] ?>"placeholder="Youtube link">
+                                            </div>
+                                            <div class="form-group col-md-6">
+                                              <label for="twitter_link ">Twitter link</label>
+                                              <input type="text" class="form-control" id="twitter_link" name="twitter_link" value="<?php echo $data['twitter_link'] ?>"placeholder="Twitter link">
+                                            </div>
+                                            <div class="form-group col-md-6">
+                                              <label for="instagram_link ">Instagram link</label>
+                                              <input type="text" class="form-control" id="instagram_link" name="instagram_link" value="<?php echo $data['instagram_link'] ?>"placeholder="Instagram link">
+                                            </div>
+                                            
+                                            <input type="hidden" value="<?php echo $data['id'] ?>" name="id">
+                                            
+                                            <div class="form-group col-md-12">
+                                              <label for="about_us">About Us</label>
+                                              <textarea class="form-control" id="about_us" name="about_us" rows="10" cols="30" ><?php echo $data['about_us'] ?></textarea>
+                                            </div>
+                                            <div class="form-group ">
+                                              <label>File upload</label>
+                                              <input type="file" name="temple_logo" class="file-upload-default">
+                                              <div class="input-group col-xs-12">
+                                                <input type="file" name="temple_logo" class="form-control file-upload-info"  placeholder="Upload Image">
+                                            </div>
+                                            <button type="submit" class="btn btn-gradient-primary mr-2 col-md-6">Submit</button>
+                                            
+                                          </form>
+                                        </div>
+                                      </div>
                                     </div>
+                                    
                                   </div>
-                      <div class="col-md-6">
-                        <label for="adress" class="form-label">Address</label>
-                        <input type="text" class="form-control" id="address" name="address" required>
-                        <div class="valid-feedback">
-                                    </div>
-                                  </div>
-                                  <div class="col-md-6 my-4">
-                                <label for="facebook_link" class="form-label">Facebook Link</label>
-                                <div class="input-group has-validation">
-                                 <input type="text" class="form-control" id="facebook_link" name="facebook_link" aria-describedby="inputGroupPrepend" required>
-                                   </div>
                                 </div>
-                                 <div class="col-md-6 my-4">
-                                   <label for="youtube_link" class="form-label">Youtube Link</label>
-                                  <div class="input-group has-validation">
-                                 <input type="text" class="form-control" id="youtube_link" name="youtube_link" aria-describedby="inputGroupPrepend" required>
-                                </div>
-                                  </div>
-                                 <div class="col-md-6 my-4">
-                                <label for="twitter_link" class="form-label">Twitter Link</label>
-                                  <div class="input-group has-validation">
-                               <input type="text" class="form-control" id="twitter_link" name="twitter_link" aria-describedby="inputGroupPrepend" required>
-                              </div>
-                               </div>
-                              <div class="col-md-6 my-4">
-                              <label for="instagram_link" class="form-label">Instagram Link</label>
-                                 <div class="input-group has-validation">
-                                  <input type="text" class="form-control" id="instagram_link" name="instagram_link" aria-describedby="inputGroupPrepend" required>
-                                 </div>
-                                  </div>
-                                  
-                                  <div class="col-md-12 my-4">
-                                    <label for="about_us" class="form-label">About us </label>
-                                    <textarea name="about_us" id="about_us" cols="30" rows="10"></textarea>
-                                  </div>
-                                  <div class="col-md-6 my-1">
-                                   <label class="file">Temple Logo</label><br>
-                                    <input type="file" id="file" aria-label="File browser example" name="temple_logo">
-                                    <span class="file-custom"></span>
-                                    </label>
-                                    </div>
-                                  <div class="col-12 my-4">
-                                    <button class="btn btn-primary" type="submit">Add Details</button>
-                                  </div>
-                                </form>
+                                {{-- Form Template  --}}
 
 
+
+                                @if($errors->any())
+                                <script type="text/javascript">
+                                    var error = "{{$errors->first()}}";
+                                    toastr.error(error);
+                                </script>
+                            @endif
+                            @if(session()->has('message'))
+                                <script type="text/javascript">
+                                    var message = "{{session()->get('message')}}";
+                                    toastr.success(message);
+                                </script>
+                            @endif
+                            @if (session()->has('error'))
+                                <script type="text/javascript">
+                                    var error = "{{session()->get('error')}}";
+                                    toastr.error(error);
+                                </script>
+                            @endif
                                 <script>
                                      CKEDITOR.replace('about_us');
                                  </script>
