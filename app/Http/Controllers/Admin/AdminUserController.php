f94f83cbf6de9f48d91085f03b8c8e6f81dc50c9
rk-2000 <ritikshri19@gmail.com>
2021-10-07 13:31:46 +0530

    status is checked on login

--- app/Http/Controllers/Admin/AdminUserController.php
+++ app/Http/Controllers/Admin/AdminUserController.php
@@ -11,14 +11,16 @@ use Illuminate\Support\Facades\Validator;
 
 class AdminUserController extends Controller
 {
-    public function addUser(){
+    public function addUser()
+    {
         $roles = Roles::all();
-        return view('admin/addUserView',['roles'=>$roles]);
+        return view('admin/addUserView', ['roles' => $roles]);
     }
 
-    public function addUserData(Request $request){
-    
-        $admin = new Admin; 
+    public function addUserData(Request $request)
+    {
+
+        $admin = new Admin;
         $validator = Validator::make($request->all(), [
             'user_name' => 'required|max:100',
             'email' => 'required|email|unique:tbl_admin_users',
@@ -28,69 +30,64 @@ class AdminUserController extends Controller
         ]);
         if ($validator->fails()) {
             dd('Validation Fail');
-       } else {
+        } else {
             $current_date_time = date('Y-m-d H:i:s');
-            $admin->role_id=$request->role_id;
-            $admin->user_name=$request->user_name;
-            $admin->email=$request->email;
-            $admin->mobile=$request->mobile;
+            $admin->role_id = $request->role_id;
+            $admin->user_name = $request->user_name;
+            $admin->email = $request->email;
+            $admin->mobile = $request->mobile;
             $password = Hash::make($request->password);
-            $admin->password=$password;
-            $admin->status=$request->status;
+            $admin->password = $password;
+            $admin->status = $request->status;
             $admin->created_date_time = $current_date_time;
             $admin->save();
-            return redirect()->route('users_list')->with('message','User is Added');
-            
-           
-       }
-
-
+            return redirect()->route('users_list')->with('message', 'User is Added');
+        }
     }
 
-    public function UserData(Request $request){
-        if($request->ajax())
-        {
+    public function UserData(Request $request)
+    {
+        if ($request->ajax()) {
             $start = $request->start;
             $length = $request->length;
             $column = $request->order[0]['column'];
             $asc = $request->order[0]['dir'];
             $search = $request->search['value'];
 
-            $role = Admin::where('status','!=','2');
+            $role = Admin::where('status', '!=', '2');
 
-        
+
             $data = array();
-            
-            if(!empty($search)){
-                $where = "( name LIKE '%".$search."%' )";
+
+            if (!empty($search)) {
+                $where = "( name LIKE '%" . $search . "%' )";
                 $role->whereRaw($where);
             }
 
             if ($column == 1) {
-                $role->orderBy('id',$asc);
-             } elseif($column == 2) {
-                $role->orderBy('name',$asc);
-             } elseif($column == 3) {
-                $role->orderBy('status',$asc);
-             } elseif($column == 4) {
-                 $role->orderBy('created_date_time',$asc);
-             } 
+                $role->orderBy('id', $asc);
+            } elseif ($column == 2) {
+                $role->orderBy('name', $asc);
+            } elseif ($column == 3) {
+                $role->orderBy('status', $asc);
+            } elseif ($column == 4) {
+                $role->orderBy('created_date_time', $asc);
+            }
 
-             $rolesCount = $role->count();
-             $roles = $role->offset($start)->limit($length)->get();  
-             $filteredValue = 0;
-             
-            foreach($roles as $key => $value){
-                
+            $rolesCount = $role->count();
+            $roles = $role->offset($start)->limit($length)->get();
+            $filteredValue = 0;
+
+            foreach ($roles as $key => $value) {
+
                 $nestedValue = array();
-                $nestedValue[0] = $start+$key+1; 
+                $nestedValue[0] = $start + $key + 1;
                 $nestedValue[1] = $value->email;
-                if($value->status == 1){
+                if ($value->status == 1) {
                     $nestedValue[2] = 'Active';
-                }
-                else{
+                } else {
                     $nestedValue[2] = "Inactive";
-                }       
+                }
                 $nestedValue[3] = '<div class="dropdown">
                                     <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">
                                     Action
@@ -98,7 +95,7 @@ class AdminUserController extends Controller
                                     </span>
                                     </button>
                                     <ul class="dropdown-menu text-center">
-                                    <li><a href="http://127.0.0.1:8000/admin/edit-user?id='.$value->admin_users_id.'">Edit</a></li>
+                                    <li><a href="http://127.0.0.1:8000/admin/edit-user?id=' . $value->admin_users_id . '">Edit</a></li>
                                     <li><a href="javascript:void(0)" data-delete-link="" class="user-delete-link">Delete</a></li>
                                     </ul>
                                 </div>';
@@ -115,35 +112,38 @@ class AdminUserController extends Controller
 
 
         return view('admin/UsersView');
-
     }
 
-    public function EditUser(Request $data){
+    public function EditUser(Request $data)
+    {
         $id = $data->id;
         $roles = Roles::all();
         $datas = Admin::GetData($id);
         $data = $datas[0];
         $data->id = $id;
-        return view('admin/EditUserView',['data'=>$data,'roles' => $roles]);
+        return view('admin/EditUserView', ['data' => $data, 'roles' => $roles]);
     }
-    public function EditData(Request $request){
+    public function EditData(Request $request)
+    {
         // dd($data);
-        
+
         $this->validate($request, [
             'admin_users_id' => 'required',
-            'user_name' => 'required|max:100', 
-            'email' => 'required|email|unique:tbl_admin_users,email,'.$request->admin_users_id.',admin_users_id',
+            'user_name' => 'required|max:100',
+            'email' => 'required|email|unique:tbl_admin_users,email,' . $request->admin_users_id . ',admin_users_id',
             'mobile' => 'required',
             'status' => 'required'
         ]);
-        $admin = Admin::where('admin_users_id',$request->admin_users_id)->first(); 
-        $admin->role_id=$request->role_id;
-        $admin->user_name=$request->user_name;
-        $admin->email=$request->email;
-        $admin->mobile=$request->mobile;
-        $admin->status=$request->status;
+
+
+        $admin = Admin::where('admin_users_id', $request->admin_users_id)->first();
+        $admin->role_id = $request->role_id;
+        $admin->user_name = $request->user_name;
+        $admin->email = $request->email;
+        $admin->mobile = $request->mobile;
+        $admin->status = $request->status;
         $admin->update_date_time = date('Y-m-d H:i:s');
         $admin->save();
-        return redirect()->route('users_list')->with('message','User is updated');
-    }   
+        return redirect()->route('users_list')->with('message', 'User is updated');
+    }
 }
--- app/Http/Controllers/Admin/LoginController.php
+++ app/Http/Controllers/Admin/LoginController.php
@@ -5,40 +5,42 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+
 class LoginController extends Controller
 {
-    public function __construct()
-    {
+  public function __construct()
+  {
 
-      $this->middleware('guest:admin')->except('logout');
+    $this->middleware('guest:admin')->except('logout');
+  }
+  public function index()
+  {
+    return view('admin.AdminLoginView');
+  }
 
+  public function login(Request $request)
+  {
+    $this->validate($request, [
+      "email" => "required|exists:tbl_admin_users,email",
+      "password" => "required|min:6"
+    ]);
+
+    $credentials = $request->only('email', 'password');
+    $credentials['status'] = 1;
+
+    if (Auth::guard('admin')->attempt($credentials)) {
+
+      return redirect()->route('admin.home')->with("message", "Admin login succesfully");
+    } else {
+      dd("User is deleted");
+      return redirect()->back()->with("error", "Email or password is wrong");
     }
-    public function index()
-    {
-        return view('admin.AdminLoginView');
-    }
+  }
 
-    public function login(Request $request)
-    {
-        $this->validate($request,[
-            "email" => "required|exists:tbl_admin_users,email",
-            "password" => "required|min:6"
-          ]);
-
-          $credentials = $request->only('email','password');
-
-        if (Auth::guard('admin')->attempt($credentials)) {
-
-            return redirect()->route('admin.home')->with("message","Admin login succesfully");
-        } else {
-            return redirect()->back()->with("error","Email or password is wrong");
-        }
-    }
-
-    public function logout()
-    {
-      $route = "admin.login.get";
-      Auth::guard('admin')->logout();
-      return redirect()->route($route)->with("message","logout successfully");
-    }
+  public function logout()
+  {
+    $route = "admin.login.get";
+    Auth::guard('admin')->logout();
+    return redirect()->route($route)->with("message", "logout successfully");
+  }
 }
