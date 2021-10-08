819abc44fd032d5a7004b4d0ef4b5bcd76dd2796
rk-2000 <ritikshri19@gmail.com>
2021-10-08 15:49:46 +0530

    Roles can be deleted and all users will be deleted

--- app/Http/Controllers/Admin/AdminUserController.php
+++ app/Http/Controllers/Admin/AdminUserController.php
@@ -11,40 +11,41 @@ use Illuminate\Support\Facades\Validator;
 
 class AdminUserController extends Controller
 {
+
+    // View Add USer Form
     public function addUser()
     {
-        $roles = Roles::all();
-        return view('admin/addUserView', ['roles' => $roles]);
+        $roles = Roles::where('status','!=',2)->get();
+        return view('admin/addUserView')->with(compact('roles'));
     }
 
+    // Save User Data Into Database 
     public function addUserData(Request $request)
     {
 
         $admin = new Admin;
-        $validator = Validator::make($request->all(), [
+        $this->validate($request, [
             'user_name' => 'required|max:100',
             'email' => 'required|email|unique:tbl_admin_users',
             'mobile' => 'required',
             'password' => 'required',
             'status' => 'required'
         ]);
-        if ($validator->fails()) {
-            dd('Validation Fail');
-        } else {
-            $current_date_time = date('Y-m-d H:i:s');
-            $admin->role_id = $request->role_id;
-            $admin->user_name = $request->user_name;
-            $admin->email = $request->email;
-            $admin->mobile = $request->mobile;
-            $password = Hash::make($request->password);
-            $admin->password = $password;
-            $admin->status = $request->status;
-            $admin->created_date_time = $current_date_time;
-            $admin->save();
-            return redirect()->route('users_list')->with('message', 'User is Added');
-        }
+        $current_date_time = date('Y-m-d H:i:s');
+        $admin->role_id = $request->role_id;
+        $admin->user_name = $request->user_name;
+        $admin->email = $request->email;
+        $admin->mobile = $request->mobile;
+        $password = Hash::make($request->password);
+        $admin->password = $password;
+        $admin->status = $request->status;
+        $admin->created_date_time = $current_date_time;
+        $admin->save();
+        return redirect()->route('users_list')->with('message', 'User is Added');
     }
 
+
+    //View all users into datatable
     public function UserData(Request $request)
     {
         if ($request->ajax()) {
@@ -53,17 +54,12 @@ class AdminUserController extends Controller
             $column = $request->order[0]['column'];
             $asc = $request->order[0]['dir'];
             $search = $request->search['value'];
-
             $role = Admin::where('status', '!=', '2');
-
-
             $data = array();
-
             if (!empty($search)) {
                 $where = "( name LIKE '%" . $search . "%' )";
                 $role->whereRaw($where);
             }
-
             if ($column == 1) {
                 $role->orderBy('id', $asc);
             } elseif ($column == 2) {
@@ -83,7 +79,6 @@ class AdminUserController extends Controller
             $filteredValue = 0;
 
             foreach ($roles as $key => $value) {
-
                 $nestedValue = array();
                 $nestedValue[0] = $start + $key + 1;
                 $nestedValue[1] = $value->user_name;
@@ -105,7 +100,7 @@ class AdminUserController extends Controller
                                         </button>
                                         <ul class="dropdown-menu text-center">
                                         <li><a href="http://127.0.0.1:8000/admin/edit-user?id=' . $value->admin_users_id . '">Edit</a></li>
-                                        <li><a href="javascript:void(0)" data-delete-link="" class="user-delete-link">Delete</a></li>
+                                        <li><a href="http://127.0.0.1:8000/admin/delete-user?id=' . $value->admin_users_id . '"class="user-delete-link">Delete</a></li>
                                         </ul>
                                     </div>';
                 $data[] = $nestedValue;
@@ -118,15 +113,14 @@ class AdminUserController extends Controller
             echo json_encode($json_data);
             exit;
         }
-
-
         return view('admin/UsersView');
     }
 
+    // View Existing User Data in Edit User Form
     public function EditUser(Request $data)
     {
         $id = $data->id;
-        $roles = Roles::all();
+        $roles = Roles::where('status','!=',2)->get();
         $datas = Admin::GetData($id);
         $data = $datas[0];
         $data->id = $id;
@@ -134,6 +128,7 @@ class AdminUserController extends Controller
     }
 
 
+    // Edit existing user details
     public function EditData(Request $request)
     {
 
@@ -144,7 +139,6 @@ class AdminUserController extends Controller
             'mobile' => 'required',
             'status' => 'required'
         ]);
-
         $admin = Admin::where('admin_users_id', $request->admin_users_id)->first();
         $admin->role_id = $request->role_id;
         $admin->user_name = $request->user_name;
@@ -154,5 +148,14 @@ class AdminUserController extends Controller
         $admin->update_date_time = date('Y-m-d H:i:s');
         $admin->save();
         return redirect()->route('users_list')->with('message', 'User is updated');
+    }
+
+    public function DeleteUser(Request $req)
+    {
+        $user = Admin::where('admin_users_id', $req->id)->get()->first();
+        $user->status = 2;
+        $user->email = $user->name . "_deleted";
+        $user->save();
+        return redirect()->route('user-list')->with("message", "User is deleted");
     }
 }
--- resources/views/admin/addUserView.blade.php
+++ resources/views/admin/addUserView.blade.php
@@ -1,79 +1,85 @@
 @extends('admin.layout')
 
 @section('content')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"
+    integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css">
 
 <div class="content-wrapper">
-                      <div class="page-header">
-                        <h3 class="page-title">
-                          <span class="page-title-icon bg-gradient-primary text-white mr-2">
-                            <i class="mdi mdi-border-color"></i>
-                          </span> Add User
-                        </h3>
-                      
-                      </div>
-                    
-                    
-                    {{-- FORM START --}}
-                    
-                    <form class="row  needs-validation" method="POST" action="{{route('addUserData')}}" >
-                      @csrf
-                      <div class="col-md-4">
-                        <label for="name" class="form-label">User Name</label>
-                        <input type="text" class="form-control" id="name" name="user_name" required>
-                        <div class="valid-feedback">
-                                    </div>
-                                  </div>
-                                  <div class="col-md-4">
-                                    <label for="email" class="form-label">Email</label>
-                                    <input type="email" class="form-control" id="email" name="email" required>
-                                  </div>
-                                  <div class="col-md-4">
-                                    <label for="mobile" class="form-label">Mobile Number</label>
-                                    <div class="input-group has-validation">
-                                      <input type="text" class="form-control" id="mobile" name="mobile" aria-describedby="inputGroupPrepend" required>
-                                    </div>
-                                  </div>
-                                  <div class="col-md-4 my-4">
-                                    <label for="password" class="form-label">Password </label>
-                                    <input type="checkbox" class="m-2" onclick="myFunction()">Show Password
-                                    <input type="password" class="form-control" id="password" name="password" required>
-                                  </div>
-                        
-                                  <div class="form-group col md-2 my-4">
-                                    <label for="exampleFormControlSelect1">Add Role :</label>
-                                    <select class="form-control " name="role_id" id="exampleFormControlSelect1">
-                                      @foreach ($roles as $role )
-                                      <option value="<?php echo $role['id'] ?>"><?php echo $role['name']  ?></option>
-                                      @endforeach      
-                                    </select>
-                                  </div>
-                                  <div class="form-group col md-2 my-4">
-                                    <label for="exampleFormControlSelect1">Status</label>
-                                    <select class="form-control " name="status" id="exampleFormControlSelect1">
-                                      <option value="1">Active</option>
-                                      <option value="0">Inactive</option>
-                                    </select>
-                                  </div>         
-                                  <div class="col-12 my-2">
-                                    <button class="btn btn-primary" type="submit">Add User</button>
-                                  </div>
-                                </form>
-                                @if($errors->any())
+<div class="page-header">
+<h3 class="page-title">
+<span class="page-title-icon bg-gradient-primary text-white mr-2">
+<i class="mdi mdi-border-color"></i>
+</span> Add User
+</h3>
+
+</div>
+
+
+{{-- FORM START --}}
+
+<form class="row  needs-validation" method="POST" action="{{route('addUserData')}}" >
+@csrf
+<div class="col-md-4">
+<label for="name" class="form-label">User Name</label>
+<input type="text" class="form-control" id="name" name="user_name" required>
+<div class="valid-feedback">
+        </div>
+      </div>
+      <div class="col-md-4">
+        <label for="email" class="form-label">Email</label>
+        <input type="email" class="form-control" id="email" name="email" required>
+      </div>
+      <div class="col-md-4">
+        <label for="mobile" class="form-label">Mobile Number</label>
+        <div class="input-group has-validation">
+          <input type="text" class="form-control" id="mobile" name="mobile" aria-describedby="inputGroupPrepend" required>
+        </div>
+      </div>
+      <div class="col-md-4 my-4">
+        <label for="password" class="form-label">Password </label>
+        <input type="checkbox" class="m-2" onclick="myFunction()">Show Password
+        <input type="password" class="form-control" id="password" name="password" required>
+      </div>
+
+      <div class="form-group col md-2 my-4">
+        <label for="exampleFormControlSelect1">Add Role :</label>
+        <select class="form-control " name="role_id" id="exampleFormControlSelect1">
+          @foreach ($roles as $role )
+          <option value="<?php echo $role['id'] ?>"><?php echo $role['name']  ?></option>
+          @endforeach      
+        </select>
+      </div>
+      <div class="form-group col md-2 my-4">
+        <label for="exampleFormControlSelect2">Status</label>
+        <select class="form-control " name="status" id="exampleFormControlSelect2">
+          <option value="1">Active</option>
+          <option value="0">Inactive</option>
+        </select>
+      </div>         
+      <div class="col-12 my-2">
+        <button class="btn btn-primary" type="submit">Add User</button>
+      </div>
+    </form>
+    @if($errors->any())
+      
       <script type="text/javascript">
           var error = "{{$errors->first()}}";
           toastr.error(error);
       </script>
-  @endif
-  @if(session()->has('message'))
-      <script type="text/javascript">
-          var message = "{{session()->get('message')}}";
-          toastr.success(message);
-      </script>
-  @endif
-  @if (session()->has('error'))
-      <script type="text/javascript">
-          var error = "{{session()->get('error')}}";
-          toastr.error(error);
-      </script>
-  @endif
-@endsection
+    @endif
+    @if(session()->has('message'))
+        <script type="text/javascript">
+            var message = "{{session()->get('message')}}";
+            toastr.success(message);
+        </script>
+    @endif
+    @if (session()->has('error'))
+        <script type="text/javascript">
+            var error = "{{session()->get('error')}}";
+            toastr.error(error);
+        </script>
+    @endif
+  @endsection
